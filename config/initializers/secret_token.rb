8d8a729849c0481be58163fb263a1a45cfc2e7b5
Bence Vajda <bence.vajda@me.com>
2013-04-15 01:04:40 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapMatifou::Application.config.secret_token = '1c35169fabb44d12f0aa8589cec53de3b9e50ca59357764522340b49528dda1fa2cc5acd202fc49719fac9f3956e09baabf5b82928775ec12b170de3ab00403e'
